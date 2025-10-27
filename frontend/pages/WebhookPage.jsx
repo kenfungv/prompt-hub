59daec8075e17e7633900862e9c05f4da41c3d46
kenfungv <31901015+kenfungv@users.noreply.github.com>
2025-10-27 10:44:37 +0800

    Create WebhookPage.jsx

---
+++ frontend/pages/WebhookPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import WebhookSettings from '../components/WebhookSettings';
+
+const WebhookPage = () => (
+  <div>
+    <h1>Webhook 管理中心</h1>
+    <WebhookSettings />
+  </div>
+);
+
+export default WebhookPage;
